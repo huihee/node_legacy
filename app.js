d4b9d0f847fc5bfec4d43585e467e3a2b203e682
CHJ <huihee@g.hongik.ac.kr>
2025-01-13 16:16:30 +0900

    docs : app.js 주석 추가

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const express = require('express') // Express
+/*==========================================================
+ * require('express') : Express 모듈을 불러온다.
+ * express() : Express 앱 객체를 생성한다.
+ * app-get : 특정 경로에 대해 요청-응답 동작을 정의한다.
+ * app.listen : 지정된 포트에서 서버를 시작하고 요청을 수신한다.
+ ==========================================================*/
+
+
+// Express 모듈을 가져온다.
+const express = require('express')
+
+// Express 애플리케이션 객체를 생성한다.
 const app = express()
 const port = 3000
 
-
-app.get('/', (req, res) => {
+// HTTP GET 요청이 '/' 경로로 들어왔을 때 실행될 라우트를 정의한다.
+// 클라이언트가 '/' 경로로 요청을 보내면
+app.get('/', (req, res) => { // 
     console.log('Got a GET request from Client')
-    res.send('Hello World')
+    res.send('<H1>Hello</H1>') // 클라이언트에게 "Hello World"라는 응답을 보낸다.
 })
 
 app.post('/', (req, res) => {
@@ -23,6 +35,9 @@ app.delete('/user', (req, res) => {
     res.send('Hello World')
 })
 
+// 서버를 3000번 포트에서 실행한다.
+// 서버가 성공적으로 실행되면 console에 메시지 출력
 app.listen(port, () => {
     console.log(`Node Legacy App listening on port ${port}`)
 })
+
