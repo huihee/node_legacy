5193bc473333a14d43303c96c06f868708249f51
CHJ <huihee@g.hongik.ac.kr>
2025-01-13 15:25:04 +0900

    test : 각종 라우트 함수의 HTTP 메서드 기본 작동 테스트

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-const express = require('express')
-const app = express() 
+const express = require('express') // Express
+const app = express()
+const port = 3000
+
 
 app.get('/', (req, res) => {
+    console.log('Got a GET request from Client')
     res.send('Hello World')
 })
 
-app.listen(3000)
+app.post('/', (req, res) => {
+    console.log('Got a POST request from Client')
+    res.send('Hello World')
+})
+
+app.put('/user', (req, res) => {
+    console.log('Got a PUT request from Client')
+    res.send('Hello World')
+})
+
+app.delete('/user', (req, res) => {
+    console.log('Got a DELETE request from Client')
+    res.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log(`Node Legacy App listening on port ${port}`)
+})
