0010a38dda3d1d8817874b42aa90fc1309cdb732
CHJ <huihee@g.hongik.ac.kr>
2025-01-14 14:46:47 +0900

    chore : 메인 어플리케이션이 바디파서를 사용하도록 설정

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+// 설치된 외부 라이브러리를 불러오기 위해 require() 사용
 const express = require('express')
 const ejs = require('ejs')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
@@ -7,6 +9,10 @@ app.set('view engine', 'ejs')
 app.set('viess', './views')
 // static file serving
 app.use(express.static(__dirname+'/public'))
+// parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false })) // bodyParser가 해당 형식을 파싱해줌
+// parsing JSON
+app.use(bodyParser.json()) // bodyParser가 해당 형식을 파싱해줌
 
 app.get('/', (req, res) => { 
     res.render('index'); 
@@ -20,10 +26,12 @@ app.get('/users', (req, res) => {
     res.render('users');
 })
 
+// 뷰 페이지를 렌더링 하는 렌더링 설정, 라우터 함수 작성
 app.get('/contact', (req, res) => { 
     res.render('contact');
 })
 
+// post 설정
 app.post('/api/contact', (req, res) => {
     const name = req.body.name;
     const phone = req.body.phone;
@@ -32,7 +40,7 @@ app.post('/api/contact', (req, res) => {
 
     const data = `${name} ${phone} ${email} ${memo}`
 
-    res.send(data)
+    res.send(data) // data를 응답으로
 })
 
 app.listen(port, () => {
