390fc21363d6540cf1072b03e5ef7b69bb96226e
CHJ <huihee@g.hongik.ac.kr>
2025-01-15 14:16:21 +0900

    refactor : jQuery의 Ajax를 통한 HTTP Method 변경 및 요청 스크립트 추가

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.get('/contactList', (req, res) => {
     })
 });
 
-app.post('/api/contactDelete/:id', (req, res) => {
+app.delete('/api/contactDelete/:id', (req, res) => {
     const id = req.params.id;
     const deleteQuery = `DELETE FROM contact WHERE ID='${id}'`
     connectionPool.query(deleteQuery, (err, result) => {
@@ -112,7 +112,7 @@ app.post('/api/contactDelete/:id', (req, res) => {
     })
 })
 
-app.post('/api/contactUpdate/:id', (req, res) => {
+app.put('/api/contactUpdate/:id', (req, res) => {
     const id = req.params.id;
     const status = "done";
     const updateQuery = `UPDATE contact SET status='${status}' WHERE id='${id}'`
